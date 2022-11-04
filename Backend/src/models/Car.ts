72769374c90ce55cf8d0ae24e6053002ea528ec4
ahsanali17 <ahsantime1@gmail.com>
2022-11-04 19:11:31 -0400

    refactored code according to Sujatas advice and changed some column names for car

--- Backend/src/models/Car.ts
+++ Backend/src/models/Car.ts
@@ -2,10 +2,10 @@ import mongoose, { Document, Schema } from "mongoose";
 
 export interface ICars {
  car_brand: string;
- car_type: string;
+ type_car: string;
  car_name: string;
- number_of_seats: number;
- maximum_gas: number;
+ seat_capacity: number;
+ maximum_gasoline: number;
 }
 
 export interface ICarsModel extends ICars, Document {};
@@ -13,10 +13,10 @@ export interface ICarsModel extends ICars, Document {};
 const CarsSchema: Schema = new Schema(
  {
   car_brand: { type: String, required: true },
-  car_type: { type: String, required: true },
+  type_car: { type: String, required: true },
   car_name: { type: String, required: true },
-  number_of_seats: { type: Number, required: true },
-  maximum_gas: { type: Number, required: true }
+  seat_capacity: { type: Number, required: true },
+  maximum_gasoline: { type: Number, required: true }
  },
  {
   versionKey: false
