f18442771e6c727b48444b4c5d74eaf32a394b61
Tidbits JS <tidbitsjs@gmail.com>
2022-11-07 20:52:20 +0530

    typescript fix

--- Frontend/src/contexts/CarContext.tsx
+++ Frontend/src/contexts/CarContext.tsx
@@ -1,64 +1,79 @@
-import React, { createContext, useReducer, useEffect } from "react"
+import React, { createContext, useReducer, useEffect } from "react";
 
-const CarsContext = createContext({})
-const { Provider } = CarsContext
+const CarsContext = createContext({});
+const { Provider } = CarsContext;
 // const REDUCERCONSTANTS = {
 //   GET_ALL_CARS: "GET_ALL_CARS",
 // }
 type CarsContextProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
+
 type CarType = {
-  car_brand: string
-  type_car: string
-  car_name: string
-  seat_capacity: number
-  maximum_gasoline: number
-}[]
+  car_brand: string;
+  type_car: string;
+  car_name: string;
+  seat_capacity: number;
+  maximum_gasoline: number;
+};
+
 type State = {
-  cars: CarType
-  cartItems: string[]
+  cars: Array<CarType>;
+  cartItems: string[];
+};
+
+enum ActionKind {
+  GetAllCars = "GET_ALL_CARS",
+  GetOneCar = "GET_One_CAR",
 }
 
 type Action = {
-  type: "GET_ALL_CARS" | "GET_ONE_CAR"
-  payload: string[]
-}
+  type: ActionKind;
+  payload: Array<CarType>;
+};
 
-const initialState:any = {
+// type Action = {
+//   type: "GET_ALL_CARS" | "GET_ONE_CAR";
+//   payload: Array<CarType>;
+// };
+
+const initialState: State = {
   cars: [],
   cartItems: [],
-}
-function carsReducer(state: any, action: any) {
+};
+
+function carsReducer(state: State, action: Action): State {
   switch (action.type) {
-    case "GET_ALL_CARS":
-      return [...state.cars, action.payload]
+    case ActionKind.GetAllCars:
+      return { ...state, cars: action.payload };
+    default:
+      return state;
   }
 }
 
 function CarsContextProvider({ children }: CarsContextProps) {
-  const [state, dispatch] = useReducer(carsReducer, initialState)
-  console.log(state)
+  const [state, dispatch] = useReducer(carsReducer, initialState);
+  console.log(state);
 
   useEffect(() => {
     const fetchcars = async (): Promise<void> => {
       try {
-        const response = await fetch("http://localhost:4000/get/")
-        const data: CarType = await response.json()
+        const response = await fetch("http://localhost:4000/get/");
+        const data = await response.json();
         if (response.ok) {
-          dispatch({ type: "GET_ALL_CARS", payload: data })
+          dispatch({ type: ActionKind.GetAllCars, payload: data });
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    fetchcars()
-  }, [state])
+    };
+    fetchcars();
+  }, [state]);
 
   return (
     <div>
-      <Provider value={{...state,dispatch}}>{children}</Provider>
+      <Provider value={{ ...state, dispatch }}>{children}</Provider>
     </div>
-  )
+  );
 }
-export { CarsContextProvider, CarsContext }
+export { CarsContextProvider, CarsContext };
